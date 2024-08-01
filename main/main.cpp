1380c8f0b5355cb0c52bd0c56e5a1b772a6c9e37
ThanaloekKaisai <65030095@kmitl.ac.th>
2024-08-01 16:08:59 +0700

    update_led

--- main/main.cpp
+++ main/main.cpp
@@ -12,52 +12,30 @@ LED led6(21);
 LED led7(22); 
 LED led8(23); 
 
-void ALL_LED_OFF()
-{
-    led1.OFF();
-    led2.OFF();
-    led3.OFF();
-    led4.OFF();
-    led5.OFF();
-    led6.OFF();
-    led7.OFF();
-    led8.OFF();
-}
+LED leds[] = {led1, led2, led3, led4, led5, led6, led7, led8};
 
 extern "C" void app_main(void)
 {
     int i = 0;
+    int direction = 1; // 1 for forward, -1 for backward
+
     while(1)
-    {        
-        switch(i)
+    {
+        leds[i].ON();
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        leds[i].OFF();
+
+        // Move to the next LED
+        i += direction;
+
+        // Check if we need to change direction
+        if (i == 7) // reached the end, switch direction to backward
         {
-            case 0:
-            led1.ON();
-            break;
-            case 1:
-            led2.ON();
-            break;
-            case 2:
-            led3.ON();
-            break;
-            case 3:
-            led4.ON();
-            break;
-            case 4:
-            led5.ON();
-            break;
-            case 5:
-            led6.ON();
-            break;
-            case 6:
-            led7.ON();
-            break;
-            case 7:
-            led8.ON();
-            break;            
+            direction = -1;
         }
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        ALL_LED_OFF();
-        if(i++ >= 7) i = 0;
+        else if (i == 0) // reached the start, switch direction to forward
+        {
+            direction = 1;
+        }
     }
 }
